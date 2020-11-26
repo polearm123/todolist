c480db85550246b86f5e44f43ca6f66c44affd7d
polearm123 <shaggywaggy29@gmail.com>
2020-11-26 13:57:38 +0000

    created DOM manipulator functions to view projects and their respective todo Lists, added a taskbar to each project div, allowing the user to view project name andpress a button that will allow them to add a toDoTask to that project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import {Project,ToDoItem,projectsModule,displayController} from './toDoItem.js';
+// import {Project,ToDoItem,projectsModule,displayController} from './toDoItem.js';
+
+import {ToDoItem,Project,ProjectList,displayController} from './toDoItem.js';
 import { compareAsc, format } from 'date-fns'
+import './styles.css';
 
 
 const toDoOne = new ToDoItem("title","description","dueDate","priority");
+const toDoTwo = new ToDoItem("dad","help dad with dishes","date","description");
+const toDoThree = new ToDoItem("testing","testing", "high", "high");
+const project1 = new Project("first Project" , "comments only ;P");
+const project2 = new Project("second project","helping dad");
 
-// console.log(project1.getTitle());
+
+console.log(toDoOne.getTitle());
+console.log(toDoOne.getPriority());
+
+console.log(project1.getTitle());
+console.log(project1.getDescription());
+
+project1.addToDo(toDoOne);
+project2.addToDo(toDoTwo);
+project1.addToDo(toDoThree);
+project2.addToDo(toDoThree);
+
+var todoList = project1.getList();
+var todoTwo = project2.getList();
+
+console.log(todoList[0].getTitle());
+console.log(todoTwo[0].getTitle());
+
+ProjectList.addNewProject(project1);
+ProjectList.addNewProject(project2);
+
+console.log(ProjectList.listProjects()[0].getTitle());
+
+
+displayController.displayProjects();
+
+
+
+
+
+
+
 
 // const todo1 = new ToDoItem("help mum", "do dishes",new Date(2014,1,11),"high");
 
--- src/toDoItem.js
+++ src/toDoItem.js
@@ -2,28 +2,32 @@
 
 const ToDoItem = (title,description,dueDate,priority) => {
 
+    // this.title = title;
+    // this.description = description;
+    // this.dueDate = dueDate;
+    // this.priority = priority;
 
     const getTitle = () => {
 
-        return this.title;
+        return title;
 
     }
 
     const getDescription = () => {
 
-        return this.description;
+        return description;
 
     }
 
     const getDueDate = () => {
 
-        return this.dueDate;
+        return dueDate;
 
     }
 
     const getPriority = () => {
 
-        return this.priority;
+        return priority;
 
     }
 
@@ -51,41 +55,40 @@ const ToDoItem = (title,description,dueDate,priority) => {
 
     }
 
-
     return {getDescription,getDueDate,getPriority,getTitle,setPriority,setDueDate,setDescription,setTitle}
 
-};
+}
 
 
 
 const Project = (title,description) => {
 
-    this.toDoList = [];
-
+    var toDoList = [];
+    
 
     const getTitle = () => {
 
-        return this.title;
+        return title;
 
     }
 
 
     const getDescription = () => {
 
-        return this.description;
+        return description;
 
     }
 
     //TODO: respect to showing completed and non completed
     const getList = () => { 
 
-        return this.toDoList;
+        return toDoList;
 
     }
 
     const addToDo = (projectToDoElement) => {//needs to be validation of the object, within the todo class?
 
-        this.toDoList.push(projectToDoElement);
+        toDoList.push(projectToDoElement);
 
     }
 
@@ -95,33 +98,109 @@ const Project = (title,description) => {
 
 const ProjectList = (function() {
 
-    let projectList = []; //stores a list of projects for view
+    var listOfProjects = []; //stores a list of projects for view
 
 
     const addNewProject = (project) => {//creates a new project and adds it to the list
 
-        projectList.push(project);
+        listOfProjects.push(project);
 
     }
 
     const listProjects = () => { //returns a list of all current projects
 
-        return projectList;
+        return listOfProjects;
 
     }
 
     return {listProjects,addNewProject}
-
-    
 
 }());
 
 
 const displayController = (function(){
 
-    const displayProjects = () => {
+    const displayProjects = () => { //goes through the project list and prints them on screen 
 
-        console.log("not implemented yet");
+        var container = document.querySelector(".container");
+
+        for(let i=0;i<ProjectList.listProjects().length;i++){ //appends projects to thei main container
+            
+            var projectDiv = createProjectDiv(ProjectList.listProjects()[i]);
+            container.appendChild(projectDiv)
+
+        }
+
+    }
+
+    const createProjectDiv = (project) => { //creates and returns project div with correct title and ID
+
+        var toDoList = project.getList();
+        var projectTaskBar = document.createElement("div");
+        var projectElement = document.createElement("div");
+
+        projectElement.classList.add("project");
+        projectElement.id = project.getTitle();
+        
+        projectElement.appendChild(createProjectTaskBar(project));
+
+
+        for(let i=0;i<toDoList.length;i++){ //goes over the toDos in the project
+                                            //appending each one to the parent project div
+            projectElement.appendChild(createToDoDiv(toDoList[i]));
+
+        }
+
+        return projectElement;
+
+    }
+
+    const createToDoDiv = (toDoElement) => {//returns a to do card with all information to be 
+                                            //appended to their project div
+
+        var toDoDiv = document.createElement("div");
+        toDoDiv.classList.add("to-do-item");
+        
+        var description = toDoElement.getDescription();
+        var dueDate = toDoElement.getDueDate();
+        var priority = toDoElement.getPriority();
+        var title = toDoElement.getTitle();
+
+        var titlePara = document.createElement("p");
+        titlePara.textContent = `title: ${title}`;
+        var descriptionPara = document.createElement("p");
+        descriptionPara.textContent = `description: ${description}`;
+        var dueDatePara = document.createElement("p");
+        dueDatePara.textContent = `due date: ${dueDate}`;
+        var priorityPara = document.createElement("p");
+        priorityPara.textContent = `priority ${priority}`;
+
+        toDoDiv.appendChild(titlePara);
+        toDoDiv.appendChild(descriptionPara);
+        toDoDiv.appendChild(dueDatePara);
+        toDoDiv.appendChild(priorityPara);
+
+        return toDoDiv;
+
+    }
+
+    const createProjectTaskBar = (project) => {//returns a div with the project task/nav bar
+
+        var projectTaskBarDiv = document.createElement("div");
+        projectTaskBarDiv.classList.add("project-task-bar");
+
+        var title = document.createElement("h1");
+        title.textContent = `Title: ${project.getTitle()}`;
+
+        var addButton = document.createElement("button");
+        addButton.classList.add("add-button");
+        addButton.id = project.getTitle();
+        addButton.textContent = "add task";
+
+        projectTaskBarDiv.appendChild(title);
+        projectTaskBarDiv.appendChild(addButton);
+
+        return projectTaskBarDiv;
 
     }
 
@@ -133,15 +212,8 @@ const displayController = (function(){
 
     return {displayToDoLists,displayProjects}
 
-})();
+})()
 
 
 // export {Project,ToDoItem,ProjectList,displayController}
-
-
-
-
-
-const project1 = new Project("hello", "hello");
-
-console.log(project1);
+export {Project,ToDoItem,ProjectList,displayController}
